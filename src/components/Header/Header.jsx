b4bd4e2a1dd07e6c3dcc8aa0b27bb8c088c30b22
Mohammed Imaduddin <123477562+mdimado@users.noreply.github.com>
2024-02-05 14:54:26 +0530

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,54 +1,72 @@
-import React, {useRef,useEffect} from "react";
-
+import React, { useRef, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import './header.css';
-import logo2 from '../../assets/images/opensys-logo.png'
-import logo from '../../assets/images/LogoCOSC.svg'
+import logo2 from '../../assets/images/opensys-logo.png';
+import logo from '../../assets/images/LogoCOSC.svg';
 import { Container, Row } from "reactstrap";
-
 import { Link } from "react-router-dom";
 
 const Header = () => {
-
   const headerRef = useRef(null);
-  
-  
-  
- 
-
   const menuRef = useRef(null);
   const navigate = useNavigate();
- 
 
-  
+  const menuToggle = () => menuRef.current.classList.toggle('active__menu');
 
+  useEffect(() => {
+    const stickyHeaderFunc = () => {
+      window.addEventListener('scroll', () => {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+          headerRef.current.classList.add('sticky__header');
+        } else {
+          headerRef.current.classList.remove('sticky__header');
+        }
+      });
+    };
 
- 
- 
+    stickyHeaderFunc();
+    return () => window.removeEventListener('scroll', stickyHeaderFunc);
+  }, []);
 
-  return <header className="header" ref={headerRef}>
-    <Container>
-      <Row>
-        <div className="nav__wrapper">
-        <div className="cosclogo">
-            <Link to='/home'><img src={logo} alt="logo" /></Link>
-            <p>|</p>
-            <Link to='/home'><img src={logo2} alt="logo" /></Link>
-          </div>
-
-
-           
-
-            <div className="nav__icons">
-                 
+  return (
+    <header className="header" ref={headerRef}>
+      <Container>
+        <Row>
+          <div className="nav__wrapper">
+            <div className="cosclogo">
+              <Link to='/home'><img src={logo} alt="logo" /></Link>
+              <p>|</p>
+              <Link to='/home'><img src={logo2} alt="logo" /></Link>
             </div>
 
-            
+            <div className="navigation" ref={menuRef} onClick={menuToggle}>
+              <ul className="menu">
+                <li className="nav__item">
+                  <a href="#hero_content">Home</a>
+                </li>
+                <li className="nav__item">
+                  <a href="#about">About</a>
+                </li>
+                <li className="nav__item">
+                  <a href="#events">Events</a>
+                </li>
+                <li className="nav__item">
+                  <a href="#faq">FAQs</a>
+                </li>
+                <li className="nav__item">
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
+            </div>
 
-        </div>
-      </Row>
-    </Container>
-  </header>
+            <div className="mobile__menu">
+              <span onClick={menuToggle}><i className="ri-menu-line"></i></span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,36 @@ import { motion } from "framer-motion";
 import Helmet from "../components/Helmet/Helmet";
 import "../styles/home.css";
 import { Container, Row, Col } from "reactstrap";
-
+import animationData from "../assets/lottie/Animation - 1707063924246.json";
 import opensyslogo from '../assets/images/opensys-logo.png'
+import Lottie from "react-lottie";
+import laptop from '../assets/images/3d cartoon style laptop with infographic elements on screen icon.png'
+import code from '../assets/images/png-transparent-coding-folder-programming-folder-coding-programming-folder-code-3d-icon-removebg-preview.png'
+import bag from '../assets/images/school-bag-5412549-4537744.png'
+import penholder from '../assets/images/3d-illustration-pencil-ruler-and-school-supplies-png.webp'
+import transparent from '../assets/images/transparent.png'
+import github from '../assets/images/4695723.png'
+import cascade from '../assets/images/download.png'
 
 const Home = () => {
+
+  const lottieOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const floatingAnimation = {
+    y: [0, -20, 0], 
+    transition: {
+      duration: 3, 
+      repeat: Infinity, 
+      ease: 'linear', 
+    },
+  }
  
   return (
     <Helmet title={"Home"}>
@@ -31,16 +57,15 @@ A grand symphony of expansive technical spectacles!
 
                 <motion.button
                   className="buy__button"
-                  
                 >
-                  Register
+                  <a href="#events">Register</a>
                 </motion.button>
               </div>
             </Col>
 
             <Col lg="6" md="6">
-              <div className="hero__img">
-                <img className="opensys-logo" src={opensyslogo} alt="" />
+              <div className="lottie">
+                <Lottie options={lottieOptions} />
               </div>
               
             </Col>
@@ -51,8 +76,66 @@ A grand symphony of expansive technical spectacles!
       </section>
 
       <section id="about" className="about">
+        <div className="abouts">
+          
+          <div className="h21 h2bold"><h2  >About the Event</h2></div>
+          <div className="aboutdescription"><Col lg = '6'>
+          <p>OpenSys is an amalgamation of multiple events that are designed to give you the best competitive experience all while being fun and enjoyable. Since we are the open source community of CBIT, all the events will be completely free for everyone to participate and you also stand a chance to win exciting cash prizes. See you there!</p>
+          <div className="display-flex"><i class="ri-team-fill"></i><h5>Free for everyone to participate</h5></div>
+          <div className="display-flex"><i class="ri-trophy-fill"></i><h5>Win exciting prizes</h5></div>
+          </Col>
+          <Col className="mt-40"  lg = '6'>
+          <div className="display-flex where_when"><i class="ri-time-fill"></i><h5>When?</h5></div>
+          <div className="pr40"><p>February 26th and 27th, 2024</p></div>
+          <div className="display-flex where_when"><i class="ri-map-pin-fill"></i><h5>Where?</h5></div>
+          <div className="pr40"><p>Chaitanya Bharathi Istitute of Technology</p></div>
+          </Col></div>
+          
+          
+        
+        </div>
+       
+        
+        
+      </section>
+      
+      <section id="events" className="events">
+          <h1 className="h1events">Events</h1>
+          <div className="neomorphism">
+              <div className="containeron">
+                  <h1 >Git Cryptex</h1>
+                  <p>An event aimed to test participants' knowledge on open-source technologies such as Git and GitHub. The event comprises two stages. First, all the teams will be required to solve a crossword puzzle based on open-source technologies. After completing the crossword, based on the answers obtained, the teams must commit or push the files listed in the crossword answers to COSC’s GitHub repository.</p>
+                  <div className="display-flex icon-text"><i class="ri-team-fill"></i><p>Participation: Team Event (Team Size: 2)</p></div>
+                  <div className="display-flex icon-text"><i class="ri-calendar-todo-fill"></i><p>Date: 24th February 2024</p></div>
+                  <motion.button
+                  className="buy__button mt-40"
+                >
+                  <a href="#events">Register</a>
+                </motion.button>
+              </div>
+              <div><img className="github" src={github} alt="" /></div>
+          </div>
+         
+          <div className="neomorphism">
+              <div><img className="cascade" src={cascade} alt="" /></div>
+              <div className="containeron">
+                  <h1 >Cascade Arcade</h1>
+                  <p>An open-sourced event for all the design and UI/UX enthusiasts of the college community, who will compete against each other to create CSS templates that match the specified pattern.</p>
+                  <div className="display-flex icon-text"><i class="ri-user-3-fill"></i><p>Participation: Individual Event</p></div>
+                  <div className="display-flex icon-text"><i class="ri-calendar-todo-fill"></i><p>Date: 24th February 2024</p></div>
+                  <div className="display-flex icon-text"><i class="ri-link"></i><p>Reference Link: <a href="https://cssbattle.dev/">CSS Battle</a></p></div>
+                  <motion.button
+                  className="buy__button mt-40 mb-40"
+                >
+                  <a href="#events">Register</a>
+                </motion.button>
+              </div>
+          </div>
+          
       </section>
 
+      <section id="FAQs" className="FAQs">
+      </section>
       
     </Helmet>
   );
