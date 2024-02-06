70c92fae72cbad2d199f293f78d73463f2642a1f
Vasita27 <vasita.puppala@gmail.com>
2024-02-06 13:47:50 +0530

    contact page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,20 +14,44 @@ const Footer = () => {
         <Row>
           <Col className="text-center social" lg="12">
             <p className="socialp">
+              <div class='oo'>
+            CONTACT  INFORMATION: <br /> <br /></div><br />
+            <div class='hi'>
+            <p>LOCATION<br />Chaitanya Bharathi Institute of Technology, <br />Gandipet, Hyderabad - 500075.</p>
+            <div class='hii'>
+            <p>VISIT US: <br /><a href="https://cbitosc.github.io/">CBIT Open Source Community</a><br /> <br />Email: <br /><a href="mailto:cosc@cbit.ac.in">cosc@cbit.ac.in</a></p>
+            </div><br />
+            <div class='hiii'>
+            <p>CONTACT: <br />Contact1:xxxxxxxxxx <br /> Contact2:xxxxxxxxx</p>
+            </div>
+            <div class='yo'>
+            <p> Miscellaneous  links:  <br /></p><a href="https://cbit-hacktoberfest23.vercel.app/"> 1.hactoberfest 2023</a><br /> <a href="https://www.cbit.ac.in/">2.CBIT</a>
+            </div>
+            </div><br />
+            <br />
+            <div class='sm'>
+            <p>Our Social Media Handles: <br /> <br /></p> </div>
+
+              <div class='one'>
               <a
                 href="https://www.instagram.com/cbitosc/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
+          
                 <i className="ri-instagram-line"></i>
               </a>
-              <a
+              </div>
+              <div class='three'>
+              <a 
                 href="https://www.facebook.com/cbitosc/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <i className="ri-facebook-circle-fill"></i>
               </a>
+              </div>
+              <div class='two'>
               <a
                 href="https://twitter.com/cbitosc?t=rWGqPsaidSTGfA8FpwPPUw&s=09"
                 target="_blank"
@@ -35,6 +59,9 @@ const Footer = () => {
               >
                 <i className="ri-twitter-fill"></i>
               </a>
+              </div>
+              <div class='four'>
+
               <a
                 href="https://www.linkedin.com/company/cbitosc/"
                 target="_blank"
@@ -42,6 +69,7 @@ const Footer = () => {
               >
                 <i className="ri-linkedin-line"></i>
               </a>
+              </div>
             </p>
           </Col>
 
