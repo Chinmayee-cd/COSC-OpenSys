1e353ad2b94301033415aad0411652d2e6cff62b
Mohammed Imaduddin <123477562+mdimado@users.noreply.github.com>
2024-02-07 22:06:34 +0530

    Update Faqs.jsx

--- src/components/FAQs/Faqs.jsx
+++ src/components/FAQs/Faqs.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Accordian from "./Accordian";
-import { faqs } from "../constants/data";
+import React from 'react'
+import Accordian from './Accordian'
+import { faqs } from '../constants/data';
 
 const Faqs = () => {
-	return (
-		<section className="section w-[70%]">
-			<style>
-				{`
+  return (
+    <section id='faqs' className='section   ' >
+      <style>
+        {`
           .faqs {
             background: rgba(255, 255, 255, 0.2);
             border-radius: 16px;
@@ -14,37 +14,27 @@ const Faqs = () => {
             backdrop-filter: blur(8.7px);
             -webkit-backdrop-filter: blur(8.7px);
             border: 1px solid rgba(255, 255, 255, 0.3);
+            width:80%;
           }
 
         
           @media only screen and (max-width: 767px) {
             .faqs {
-              width: 100%;
+              width: 100% !important;
             }
           }
         `}
-			</style>
-			<div className="flex flex-col items-center">
-				<h1 className="text-white text-3xl font-sans font-bold py-10">
-					{" "}
-					FREQUENTLY ASKED QUESTIONS{" "}
-				</h1>
-				{faqs.map((faq, index) => (
-					<div
-						key={index}
-						id="faqdiv"
-						className="faqs p-4 w-[100%] rounded-lg bg-white/10 backdrop-blur-md my-2"
-					>
-						<Accordian
-							key={index}
-							question={faq.question}
-							answer={faq.answer}
-						/>
-					</div>
-				))}
-			</div>
-		</section>
-	);
-};
+      </style>
+    <div className='flex flex-col items-center'>
+      <h1 className=' h1events glowing-underline mb-5 h1contactmobile text-center '> Frequently Asked Questions </h1>
+      {faqs.map((faq, index) => (
+        <div key={index} id='faqdiv' className='faqs p-4 w-[100%] rounded-lg bg-white/10 backdrop-blur-md my-2'>
+            <Accordian key={index} question={faq.question} answer={faq.answer} />
+        </div>
+      ))}
+    </div>
+    </section>
+  )
+}
 
-export default Faqs;
+export default Faqs
