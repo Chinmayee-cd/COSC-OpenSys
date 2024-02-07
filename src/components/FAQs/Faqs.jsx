689b0df950dae1185ebf91668e2b6ba0713699e8
Mohammed Imaduddin <123477562+mdimado@users.noreply.github.com>
2024-02-07 20:18:17 +0530

    Update Faqs.jsx

--- src/components/FAQs/Faqs.jsx
+++ src/components/FAQs/Faqs.jsx
@@ -4,9 +4,19 @@ import { faqs } from '../constants/data';
 
 const Faqs = () => {
   return (
-    <section id='faqs' className='section w-[70%]'>
+    <section id='faqs' className='section   w-[70%]' >
       <style>
         {`
+          .faqs {
+            background: rgba(255, 255, 255, 0.2);
+            border-radius: 16px;
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+            backdrop-filter: blur(8.7px);
+            -webkit-backdrop-filter: blur(8.7px);
+            border: 1px solid rgba(255, 255, 255, 0.3);
+          }
+
+        
           @media only screen and (max-width: 767px) {
             .faqs {
               width: 100%;
@@ -17,7 +27,7 @@ const Faqs = () => {
     <div className='flex flex-col items-center'>
       <h1 className='text-white text-3xl font-sans font-bold py-10'> FREQUENTLY ASKED QUESTIONS </h1>
       {faqs.map((faq, index) => (
-        <div key={index} id='faqdiv' className='p-4 w-[100%] rounded-lg bg-white/10 backdrop-blur-md my-2'>
+        <div key={index} id='faqdiv' className='faqs p-4 w-[100%] rounded-lg bg-white/10 backdrop-blur-md my-2'>
             <Accordian key={index} question={faq.question} answer={faq.answer} />
         </div>
       ))}
