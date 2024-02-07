04260e77a2383c699337f36a38ef4df99395a0e7
Mohammed Imaduddin <123477562+mdimado@users.noreply.github.com>
2024-02-07 20:07:46 +0530

    Update Faqs.jsx, setup media query

--- src/components/FAQs/Faqs.jsx
+++ src/components/FAQs/Faqs.jsx
@@ -5,6 +5,15 @@ import { faqs } from '../constants/data';
 const Faqs = () => {
   return (
     <section id='faqs' className='section w-[70%]'>
+      <style>
+        {`
+          @media only screen and (max-width: 767px) {
+            .faqs {
+              width: 100%;
+            }
+          }
+        `}
+      </style>
     <div className='flex flex-col items-center'>
       <h1 className='text-white text-3xl font-sans font-bold py-10'> FREQUENTLY ASKED QUESTIONS </h1>
       {faqs.map((faq, index) => (
